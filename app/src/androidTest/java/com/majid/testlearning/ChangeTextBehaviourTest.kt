a1b72ea39d641270b7b1b30e86dbfa482f83f448
Majid <mirmajid360@gmail.com>
2024-07-22 16:58:17 +0530

    Initial commit

---
+++ app/src/androidTest/java/com/majid/testlearning/ChangeTextBehaviourTest.kt
@@ -0,0 +1,17 @@
+package com.majid.testlearning
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ChangeTextBehaviourTest {
+
+
+    @Test
+    fun changeTextBehaviour_EditText(){
+
+        
+    }
+
+}
---
+++ app/src/main/java/com/majid/testlearning/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.majid.testlearning
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    var button :Button? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        button = findViewById<Button>(R.id.button_next_activity)
+
+        button?.setOnClickListener {
+            val intent = Intent(this,SecondaryActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
